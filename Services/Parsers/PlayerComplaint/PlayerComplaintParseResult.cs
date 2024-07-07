10df23df0461a70501342436491b945e707d2fcb
xvdm <haxenx@proton.me>
2024-07-07 15:53:45 +0300

    Renamed Static to Id

--- Services/Parsers/PlayerComplaint/PlayerComplaintParseResult.cs
+++ Services/Parsers/PlayerComplaint/PlayerComplaintParseResult.cs
@@ -5,7 +5,7 @@ public sealed record PlayerComplaintParseResult
     public string? AuthorNickname { get; set; }
     public string? AuthorId { get; set; }
     public string? IntruderNickname { get; set; }
-    public string? IntruderStatic { get; set; }
+    public string? IntruderId { get; set; }
     public string? ViolationDescription { get; set; }
     public string? ViolationDateTime { get; set; }
     public string? ViolationProofs { get; set; }
